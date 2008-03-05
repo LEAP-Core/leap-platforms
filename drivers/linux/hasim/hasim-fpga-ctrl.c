83b2114d16b5440777e3b6b78b7c64e8e6104b4e
Michael Adler <michael.adler@intel.com>
2008-03-05 16:38:19 +0000

    CSN: CSN-platforms-57
    Change commands for FPGA access.  Commands are now:
    
      --reserve   Reserve the FPGA.
      --program   Enable programming mode.  Removes the PCIe driver, disables
                  the PCIe bus and enable's user write access to the programming
                  cable, if necesary.
      --activate  Activate the PCIe bus, load the driver and disable write access
                  to the programming cable.
      --drop-reservation
    
      --force may be specified to override another user's reservation.
    
    It is safe to switch between the "program" and "activate" states without
    dropping a reservation.
    
    Note that the program uses getopt_long() to parse arguments.  That function
    accepts the shortest unique string to specify an argument, so you can currently
    use "hasim-fpga-ctrl --p" and "hasim-fpga-ctrl --a".

--- drivers/linux/hasim/hasim-fpga-ctrl.c
+++ drivers/linux/hasim/hasim-fpga-ctrl.c
@@ -19,8 +19,9 @@
 #define USB_PROG   PREFIX "dev/usb_programming_cable0"
 #define RES_FILE   PREFIX "reservations/pcie_fpga0"
 
-#define KERNEL_MOD PREFIX "kernel/pchnl.ko"
-#define KERNEL_DEV "/dev/pchnl"
+#define KERNEL_MOD      "pchnl"
+#define KERNEL_MOD_PATH PREFIX "kernel/" KERNEL_MOD ".ko"
+#define KERNEL_DEV      "/dev/" KERNEL_MOD
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -36,18 +37,20 @@
 typedef enum
 {
     STATE_FREE,
-    STATE_DISABLE_PCI,
-    STATE_ENABLE_PCI,
+    STATE_RESERVED,
+    STATE_PCI_PROGRAM,
+    STATE_PCI_ACTIVE,
     STATE_LAST              // Must be last
 }
 FPGA_STATE_T;
 
 
-static int req_enable_pci = 0;
-static int req_disable_pci = 0;
+static int req_reserve = 0;
+static int req_program_pci = 0;
+static int req_activate_pci = 0;
 static int req_drop_reservation = 0;
-static int force_unlock = 0;
-static int query_state = 0;
+static int force = 0;
+static int query_status = 0;
 
 
 
@@ -67,11 +70,14 @@ void state_print(FILE *f, FPGA_STATE_T state)
       case STATE_FREE:
         s = "Free";
         break;
-      case STATE_DISABLE_PCI:
-        s = "PCI Disabled";
+      case STATE_RESERVED:
+        s = "Reserved";
         break;
-      case STATE_ENABLE_PCI:
-        s = "PCI Enabled";
+      case STATE_PCI_PROGRAM:
+        s = "Programming";
+        break;
+      case STATE_PCI_ACTIVE:
+        s = "Active";
         break;
       default:
         s = "Unknown";
@@ -119,7 +125,7 @@ void do_system(char *cmd, char *arg)
 //          This is to make it impossible for another program to update the
 //          state until this instance of the program exits.
 //
-void change_current_reservation(FPGA_STATE_T newState, int force)
+void change_current_reservation(FPGA_STATE_T newState)
 {
     int lockf;
     FILE *lf;
@@ -165,8 +171,12 @@ void change_current_reservation(FPGA_STATE_T newState, int force)
         exit(1);
     }
 
-    if ((oldState + 1 == newState) ||
-        ((newState == STATE_FREE) && (force || (oldState == STATE_ENABLE_PCI))))
+    if ((oldState == STATE_FREE) && (newState != STATE_RESERVED))
+    {
+        fprintf(stderr, "hasim-fpga-ctrl: Reservation required\n");
+        exit(1);
+    }
+    else
     {
         // Update the state in the file.
         rewind(lf);
@@ -177,15 +187,6 @@ void change_current_reservation(FPGA_STATE_T newState, int force)
 
         fflush(lf);
         ftruncate(lockf, ftell(lf));
-    }
-    else
-    {
-        fprintf(stderr, "hasim-fpga-ctrl: Illegal state transition (");
-        state_print(stderr, oldState);
-        fprintf(stderr, " -> ");
-        state_print(stderr, newState);
-        fprintf(stderr, ")\n");
-        exit(1);
     }
 
     //
@@ -232,6 +233,35 @@ void current_reservation_state(FILE *of)
 
 
 //
+// kernel_driver_is_loaded --
+//   Return true if the kernel driver is loaded
+//
+int kernel_driver_is_loaded()
+{
+    int is_loaded = 0;
+    char mod_name[128];
+    FILE *mf = fopen("/proc/modules", "r");
+
+    mod_name[sizeof(mod_name) - 1] = 0;
+
+    if (mf == NULL) error("Failed to open /proc/modules");
+
+    while (fscanf(mf, "%20s %*[^\n]", mod_name)> 0)
+    {
+        if (strcmp(mod_name, KERNEL_MOD) == 0)
+        {
+            is_loaded = 1;
+            break;
+        }
+    }
+
+    fclose(mf);
+
+    return is_loaded;
+}
+
+
+//
 // set_pci_state --
 //   Tell the kernel to enable or disable the FPGA's PCIe bus
 //
@@ -242,11 +272,11 @@ void set_pci_state(FPGA_STATE_T state)
 
     switch (state)
     {
-      case STATE_DISABLE_PCI:
+      case STATE_PCI_PROGRAM:
         printf("hasim-fpga-ctrl: Turing off PCIe bus...\n");
         req = '0';
         break;
-      case STATE_ENABLE_PCI:
+      case STATE_PCI_ACTIVE:
         printf("hasim-fpga-ctrl: Turing on PCIe bus...\n");
         req = '1';
         break;
@@ -254,6 +284,23 @@ void set_pci_state(FPGA_STATE_T state)
         error("Unexpected argument to set_pci_state()");
     }
 
+
+    if (state == STATE_PCI_PROGRAM)
+    {
+        //
+        // Make sure the kernel driver is unloaded
+        //
+        if (kernel_driver_is_loaded())
+        {
+            printf("hasim-fpga-ctrl: Unloading kernel driver...\n");
+            do_system("/sbin/rmmod", KERNEL_MOD_PATH);
+        }
+    }
+
+
+    //
+    // Change the PCI state
+    //
     f = open(PCIE_POWER, O_WRONLY);
     if (f == -1) error("Failed to open PCIe power control file " PCIE_POWER);
 
@@ -263,28 +310,106 @@ void set_pci_state(FPGA_STATE_T state)
     }
 
     close(f);
+
+
+    if (state == STATE_PCI_ACTIVE)
+    {
+        int trips;
+
+        //
+        // Make sure kernel driver is loaded
+        //
+        if (! kernel_driver_is_loaded())
+        {
+            printf("hasim-fpga-ctrl: Loading kernel driver...\n");
+            do_system("/sbin/insmod", KERNEL_MOD_PATH);
+        }
+
+        //
+        // Make the kernel driver accessible only to the current user
+        //
+        trips = 0;
+        while (chmod(KERNEL_DEV, 00600) != 0)
+        {
+            trips += 1;
+            if (trips == 15)
+            {
+                error("Kernel driver never built device file " KERNEL_DEV);
+            }
+            sleep(1);
+        }
+
+        chown(KERNEL_DEV, getuid(), -1);
+    }
 }
 
 
 //
-// disable_pci --
+// set_prog_cable_access --
+//   Pass true to enable and false to disable user access to the programming
+//   cable.
+//
+void set_prog_cable_access(int user_access)
+{
+    int prot = user_access ? 00666 : 00444;
+    char *msg = user_access ? "Enabling" : "Disabling";
+    struct stat statb;
+
+    if (stat(USB_PROG, &statb) == -1)
+    {
+        fprintf(stderr, "hasim-fpga-ctrl: Can't stat %s\n", USB_PROG);
+        exit(1);
+    }
+
+    if ((statb.st_mode & 00777) != prot)
+    {
+        // Protection isn't what we want.  Change it.
+        printf("hasim-fpga-ctrl: %s programming cable...\n", msg);
+        if (chmod(USB_PROG, prot) == -1)
+        {
+            fprintf(stderr, "hasim-fpga-ctrl: Failed to change protection of %s\n", USB_PROG);
+            exit(1);
+        }
+    }
+}
+
+
+
+//---------------------------------------------------------------------------
+//
+// Initiate requests
+//
+//---------------------------------------------------------------------------
+
+//
+// reserve --
+//   Reserve an FPGA.
+//
+void reserve()
+{
+    change_current_reservation(STATE_RESERVED);
+    set_prog_cable_access(0);
+}
+
+    
+//
+// program_pci --
 //   Disable the PCI bus in preparation for programming the FPGA.
 //
 //   Tasks:
 //     - Disable PCI
 //     - Grant user access to the programming cable
 //
-void disable_pci()
+void program_pci()
 {
-    change_current_reservation(STATE_DISABLE_PCI, 0);
-    set_pci_state(STATE_DISABLE_PCI);
-    printf("hasim-fpga-ctrl: Enabling programming cable...\n");
-    chmod(USB_PROG, 00666);
+    change_current_reservation(STATE_PCI_PROGRAM);
+    set_pci_state(STATE_PCI_PROGRAM);
+    set_prog_cable_access(1);
 }
 
 
 //
-// enable_pci --
+// activate_pci --
 //   Enable the PCI bus after programming it.
 //
 //   Tasks:
@@ -292,35 +417,11 @@ void disable_pci()
 //     - Enable PCI
 //     - Load the kernel driver for the FPGA
 //
-void enable_pci()
+void activate_pci()
 {
-    int trips;
-
-    change_current_reservation(STATE_ENABLE_PCI, 0);
-
-    printf("hasim-fpga-ctrl: Disabling programming cable...\n");
-    chmod(USB_PROG, 00444);
-
-    set_pci_state(STATE_ENABLE_PCI);
-
-    printf("hasim-fpga-ctrl: Loading kernel driver...\n");
-    do_system("/sbin/insmod", KERNEL_MOD);
-
-    //
-    // Make the kernel driver accessible only to the current user
-    //
-    trips = 0;
-    while (chmod(KERNEL_DEV, 00600) != 0)
-    {
-        trips += 1;
-        if (trips == 15)
-        {
-            error("Kernel driver never built device file " KERNEL_DEV);
-        }
-        sleep(1);
-    }
-
-    chown(KERNEL_DEV, getuid(), -1);
+    change_current_reservation(STATE_PCI_ACTIVE);
+    set_prog_cable_access(0);
+    set_pci_state(STATE_PCI_ACTIVE);
 }
 
 
@@ -332,25 +433,21 @@ void enable_pci()
 //     - Disable access to the programming cable
 //     - Remove the kernel driver for the FPGA
 //
-void drop_reservation(int force)
+void drop_reservation()
 {
-    change_current_reservation(STATE_FREE, force);
-
-    printf("hasim-fpga-ctrl: Disabling programming cable...\n");
-    chmod(USB_PROG, 00444);
-
-    printf("hasim-fpga-ctrl: Unloading kernel driver...\n");
-    do_system("/sbin/rmmod", KERNEL_MOD);
+    change_current_reservation(STATE_FREE);
+    set_prog_cable_access(0);
 }
 
 
 void usage()
 {
-    fprintf(stderr, "Usage: hasim-fpga-ctrl <--disable-pci |\n");
-    fprintf(stderr, "                        --enable-pci |\n");
-    fprintf(stderr, "                        --drop-reservation |\n");
-    fprintf(stderr, "                        --force-unlock>\n");
-    fprintf(stderr, "                        --state>\n");
+    fprintf(stderr, "Usage: hasim-fpga-ctrl <--reserve |\n");
+    fprintf(stderr, "                        --program |\n");
+    fprintf(stderr, "                        --activate |\n");
+    fprintf(stderr, "                        --drop-reservation\n");
+    fprintf(stderr, "                        --status>\n\n");
+    fprintf(stderr, "    --force may be specified to override other user's reservations\n");
     fprintf(stderr, "\n");
     current_reservation_state(stderr);
     exit(1);
@@ -366,12 +463,12 @@ int main(int argc, char *argv[])
     {
         static struct option long_options[] =
         {
-            { "enable-pci", 0, &req_enable_pci, 1 },
-            { "disable-pci", 0, &req_disable_pci, 1 },
+            { "program", 0, &req_program_pci, 1 },
+            { "activate", 0, &req_activate_pci, 1 },
+            { "reserve", 0, &req_reserve, 1 },
             { "drop-reservation", 0, &req_drop_reservation, 1 },
-            { "force-unlock", 0, &force_unlock, 1 },
-            { "state", 0, &query_state, 1 },
-            { "status", 0, &query_state, 1 },
+            { "force", 0, &force, 1 },
+            { "status", 0, &query_status, 1 },
             { 0, 0, 0, 0 }
         };
 
@@ -382,20 +479,20 @@ int main(int argc, char *argv[])
     //
     // Verify that one option is chosen
     //
-    if (req_enable_pci +
-        req_disable_pci +
+    if (req_reserve +
+        req_program_pci +
+        req_activate_pci +
         req_drop_reservation +
-        force_unlock +
-        query_state != 1)
+        query_status != 1)
     {
         usage();
     }
 
-    if (req_enable_pci) enable_pci();
-    if (req_disable_pci) disable_pci();
-    if (req_drop_reservation) drop_reservation(0);
-    if (force_unlock) drop_reservation(1);
-    if (query_state) current_reservation_state(stdout);
+    if (req_reserve) reserve();
+    if (req_program_pci) program_pci();
+    if (req_activate_pci) activate_pci();
+    if (req_drop_reservation) drop_reservation();
+    if (query_status) current_reservation_state(stdout);
 
     exit(0);
 }
