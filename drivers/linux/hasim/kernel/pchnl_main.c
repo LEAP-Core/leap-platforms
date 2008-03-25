ee3208ea34c3d2327bf83fc58a6dc173715c5b14
Michael Adler <michael.adler@intel.com>
2008-03-25 20:34:06 +0000

    CSN: CSN-platforms-75
    Shift for page offset was in wrong direction for Linux kernels <= 2.6.9

--- drivers/linux/hasim/kernel/pchnl_main.c
+++ drivers/linux/hasim/kernel/pchnl_main.c
@@ -582,7 +582,7 @@ pchnl_mmap(struct file *filp, struct vm_area_struct *vma)
      return remap_pfn_range(vma, vma->vm_start, vma->vm_pgoff,
                             vsize, vma->vm_page_prot);
 #else
-     return remap_page_range(vma, vma->vm_start, vma->vm_pgoff >> PAGE_SHIFT,
+     return remap_page_range(vma, vma->vm_start, vma->vm_pgoff << PAGE_SHIFT,
                              vsize, vma->vm_page_prot);
 #endif
 }
