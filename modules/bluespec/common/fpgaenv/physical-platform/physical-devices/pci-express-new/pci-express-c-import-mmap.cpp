40ea7e415130c65803692bfbd954b5703a8dc866
Elliott Fleming <kermin.fleming@intel.com>
2009-12-12 13:42:34 +0000

    CSN: CSN-platforms-340
    Adding support for unified synplify and xst build

--- modules/bluespec/common/fpgaenv/physical-platform/physical-devices/pci-express-new/pci-express-c-import-mmap.cpp
+++ modules/bluespec/common/fpgaenv/physical-platform/physical-devices/pci-express-new/pci-express-c-import-mmap.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
---
+++ modules/project/synplify.sdc
@@ -0,0 +1,3 @@
+
+# This is to keep the XST UCF files working
+define_global_attribute syn_edif_bit_format {%n<%i>}
