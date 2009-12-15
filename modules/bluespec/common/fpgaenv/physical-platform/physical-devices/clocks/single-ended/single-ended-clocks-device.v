4d964b6b2c799c84bb70b7f351266b70481e4999
Elliott Fleming <kermin.fleming@intel.com>
2009-12-15 06:44:40 +0000

    CSN: CSN-platforms-342
    Added a couple of files for simulating single ended clocks.  This change was necessary to make Bluespec's verilog build script happy, but I forgot to add these files

---
+++ modules/bluespec/common/fpgaenv/physical-platform/physical-devices/clocks/single-ended/single-ended-clocks-device.v
@@ -0,0 +1,13 @@
+module single_ended_clocks_device (clk,
+                                   rst_n,
+                                   clk_out,
+                                   rst_n_out);
+  input clk;
+  input rst_n;
+  output clk_out;
+  output rst_n_out;
+
+  assign clk_out = clk;
+  assign rst_n_out = rst_n;
+
+endmodule
