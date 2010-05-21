5a449a01b85527f5f7e8fbb1bb647ab9d921419b
Elliott Fleming <kermin.fleming@intel.com>
2010-05-21 03:02:12 +0000

    CSN: CSN-platforms-440
    Fixed the pipeline simulation build

--- modules/project/pipelines/SimulationPipeline.py
+++ modules/project/pipelines/SimulationPipeline.py
@@ -7,5 +7,4 @@ class Build(ProjectDependency):
   def __init__(self, moduleList):
     # Nothing to do here until the Bluespec build is cooked out of the 
     # top level
-
-
+    moduleList.topModule.moduleDependency['LOADER'] = ''
