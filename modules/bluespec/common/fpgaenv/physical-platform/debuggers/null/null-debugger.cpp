181225a216042235771a840fd35f9e6ff54f60b4
Michael Adler <michael.adler@intel.com>
2011-04-07 02:09:30 +0000

    CSN: CSN-platforms-512
    Move leap-fpga-ctrl to its own build tree, separate from drivers.

--- modules/bluespec/common/fpgaenv/physical-platform/debuggers/null/null-debugger.cpp
+++ modules/bluespec/common/fpgaenv/physical-platform/debuggers/null/null-debugger.cpp
@@ -16,6 +16,7 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 //
 
+#include "asim/syntax.h"
 #include "asim/provides/physical_platform_debugger.h"
 
 using namespace std;
