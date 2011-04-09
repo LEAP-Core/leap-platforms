d1f13930dc1fb63093e6180f8c88fd67c0c58eb4
Joel Emer <emer@csail.mit.edu>
2011-04-09 17:58:04 +0000

    CSN: CSN-platforms-1
    Added the autoconfig framework to platforms like the other
    packages (hopefully to soon be replaced with a simpler cleaner
    scons-based system).
    
    Also added debian package build files and configured them to
    create v11.04.
    
    A location (tools/platforms/leap-fpga-control) was created
    for leap-fpga-control, but the program there is a dummy...

---
+++ include/config.h
@@ -0,0 +1,26 @@
+/* include/config.h.  Generated from config.h.in by configure.  */
+/* include/config.h.in.  Generated from configure.in by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "leap-platforms"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "joel.emer@intel.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "ASIM leap-platforms"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "ASIM leap-platforms 0.6"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "leap-platforms"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.6"
+
+/* Version number of package */
+#define VERSION "0.6"
---
+++ tools/platforms/leap-fpga-control/leap-fpga-control.c
@@ -0,0 +1,10 @@
+// 'Hello World!' program 
+
+#include "stdio.h"
+ 
+int main()
+{
+  printf("Hello, World.\n");
+  return 0;
+}
+
