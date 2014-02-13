52eb24fc81c0363f56be8c9713d5e825cc2b714c
Michael Adler <michael.adler@intel.com>
2014-02-13 23:05:25 +0000

    CSN: CSN-leap-platforms-727
    Switch to BSD license.

--- modules/bluespec/common/fpgaenv/physical-platform/xilinx_xup/serial/xilinx-xup-serial.h
+++ modules/bluespec/common/fpgaenv/physical-platform/xilinx_xup/serial/xilinx-xup-serial.h
@@ -1,47 +1,29 @@
 //
-// Copyright (C) 2008 Intel Corporation
+// Copyright (c) 2014, Intel Corporation
+// All rights reserved.
 //
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
 //
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
 //
-
-#ifndef __PHYSICAL_PLATFORM__
-#define __PHYSICAL_PLATFORM__
-
-#include "asim/provides/serial_device.h"
-
-// ====================================================
-// HiTech Global v5 PCI-Express Based Physical Platform
-// ====================================================
-
-// This class is a collection of all physical devices
-// present on the HTG v5 Physical Platform
-typedef class PHYSICAL_DEVICES_CLASS* PHYSICAL_DEVICES;
-class PHYSICAL_DEVICES_CLASS: public PLATFORMS_MODULE_CLASS
-{
-  private:
-
-  //SERIAL_CLASS serialDevice;
-
-  public:
-
-    // constructor-destructor
-    PHYSICAL_DEVICES_CLASS(PLATFORMS_MODULE);
-    ~PHYSICAL_DEVICES_CLASS();
-
-    // accessors to individual devices
-    //SERIAL_DEVICE GetSerialDevice() { return &serialDevice; }
-};
-
-#endif
+// Neither the name of the Intel Corporation nor the names of its contributors
+// may be used to endorse or promote products derived from this software
+// without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
